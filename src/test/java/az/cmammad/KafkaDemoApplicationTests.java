4543cbb04980e18015e94e9a7530f46ce8af1c79
Jeyhun Mammadsaidov <mcmemmedd@gmail.com>
2023-09-23 18:20:42 +0400

    new commit

---
+++ src/test/java/az/cmammad/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.cmammad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
